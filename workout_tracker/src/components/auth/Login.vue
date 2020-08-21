ee761dc0e25c2b397883a56896302fd23227d272
Bicky Singh <9771641+absinghd@users.noreply.github.com>
2020-08-21 01:12:28 -0700

    google auth WIP

---
+++ workout_tracker/src/components/auth/Login.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+    <button @click="login">Login with Google</button>
+    <p v-if="feedback">{{feedback}}</p>
+</div>
+</template>
+
+<script>
+import firebase from '@/firebase/init'
+ console.log(firebase);
+export default {
+    name: 'Login',
+    data(){
+        return{
+            token: null,
+            user: null,
+            feedback: null
+        }
+    },
+    // methods: {
+    //     login(){
+    //         const provider = new firebase.auth.GoogleAuthProvider();
+    //         firebase.auth().signInWithPopup(provider).then(function(result) {
+    //             console.log(result);
+    //         // This gives you a Google Access Token. You can use it to access the Google API.
+    //         this.token = result.credential.accessToken;
+    //         // The signed-in user info.
+    //         this.user = result.user;
+    //     }).catch(error => {
+    //         // Handle Errors here.
+    //         this.feedback = error.message
+    //         })
+    //     }
+    //     }
+     }
+</script>
---
+++ workout_tracker/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pwa: {
+    name: 'workoutTracker',
+    themeColor: '#69B3FD'
+  }
+}
